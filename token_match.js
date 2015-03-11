466a22329d7fb70abe4f74c699a31cb8ba2f5fc5
zhiyanfoo <zhiyanfoo@gmail.com>
2015-03-11 16:15:19 +0800

    added greek letters to latex_lexer

--- token_match.js
+++ token_match.js
@@ -3,8 +3,11 @@
 
 function token_match()
 {
-    token_array = "+ - = ! / ( ) * ^ %";
-    token_array2 = "PLUS MINUS EQUAL FACTORIAL DIVIDE LEFT_PAREN RIGHT_PAREN MUTIPLY POWER MODULUS";
+    // token_array = "+ - = ! / ( ) * ^ %";
+    // token_array2 = "PLUS MINUS EQUAL FACTORIAL DIVIDE LEFT_PAREN RIGHT_PAREN MUTIPLY POWER MODULUS";
+    // token_array = "\\sin \\cos \\tan \\"
+    token_array = "\\alpha \\Alpha \\nu \\Nu \\beta \\Beta \\xi \\Xi \\gamma \\Gamma \\delta \\Delta \\pi \\Pi \\epsilon \\varepsilon rho \\varrho \\zeta \\Zeta \\sigma \\Sigma \\tau \\Tau \\theta \\vartheta \\Theta \\upsilon \\Upsilon \\iota \\Iota \\phi \\varphi \\Phi \\kappa \\Kappa \\chi \\Chi\\ \\lambda \\Lambda\\ \\psi \\Psi \\mu \\Mu \\omega \\Omega"
+    token_array = 
     token_array = token_array.split(" ");
     token_array2 = token_array2.split(" ");
     token_match = {}
